ddcdae5cfbe534f3569bddddfdb788680f24f8b0
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 16:44:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-//your JS code here. If required.
 // Get references to the <p> tag and the button
 const statusElement = document.getElementById("status");
 const enterButton = document.getElementById("enterBtn");
 
 // Add a click event listener to the button
 enterButton.addEventListener("click", function() {
-    // Change the text inside the <p> tag to "Entered Metaverse"
-    statusElement.textContent = "Entered Metaverse";
+    // Create a new <h1> element
+    const h1Element = document.createElement("h1");
+    
+    // Set its text content to "Entered Metaverse"
+    h1Element.textContent = "Entered Metaverse";
+    
+    // Replace the <p> tag with the new <h1> element
+    statusElement.parentNode.replaceChild(h1Element, statusElement);
 });
